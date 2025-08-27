a22dece35cb6099cb8aec2dd42cdda957b0a32a5
Kailash0319 <kailashkumavat05@gmail.com>
2025-08-27 23:38:25 +0530

    new file

---
+++ calculator.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class calculator {
+
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+         
+        scanner.close();
+
+        int sum = num1 + num2;
+        int subtraction = num1 - num2;
+        int multiplication = num1 * num2;
+        int division = num1 / num2;       // integer division
+        int remainder = num1 % num2;      // fixed: use % for remainder
+
+        System.out.println( num1 + " + " + num2 + " = " + sum);
+        System.out.println( num1 + " - " + num2 + " = " + subtraction);
+        System.out.println( num1 + " * " + num2 + " = " + multiplication);
+        System.out.println( num1 + " / " + num2 + " = " + division);
+        System.out.println( num1 + " % " + num2 + " = " + remainder);  
+
+        
+    }
+}
